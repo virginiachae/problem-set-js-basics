c665f34ce3e1649093f8e6aace3aa8f7581d7476
virginiachae <chae.virginia@gmail.com>
2016-10-26 22:16:50 -0700

    finished shakes

--- challenges/shakespearian-insult-generator.js
+++ challenges/shakespearian-insult-generator.js
@@ -11,9 +11,32 @@
   - Create a browser interface, using some combination of `prompt`, `alert`, and/or DOM manipulation.
 
 */
-
+console.log('js linked');
 var first_word = ["yeasty", "weedy", "wayward", "warped", "villainous", "venomed", "vain", "unmuzzled", "tottering", "surly", "spongy", "spleeny", "saucy", "ruttish", "roguish", "reeky", "rank", "qualling", "puny", "puking", "pribbling", "paunchy", "mewling", "mangled", "mammering", "lumpish", "loggerheaded", "jarring", "infectious", "impertinent", "gorbellied", "goatish", "gleeking", "frothy", "froward", "fobbing", "fawning", "errant", "droning", "dissembling", "dankish", "currish", "craven", "clouted", "cockered", "churlish", "bootless", "beslubbering", "bawdy", "artless"];
 var second_word = ["weather-bitten", "unchin-snouted", "toad-spotted", "tickle-brained", "tardy-gaited", "swag-bellied", "spur-galled", "sheep-biting", "shard-borne", "rump-fed", "rude-growing", "rough-hewn", "reeling-ripe", "pox-marked", "pottle-deep", "plume-plucked", "onion-eyed", "motley-minded", "milk-livered", "knotty-pated", "ill-nurtured", "ill-breeding", "idle-headed", "hell-hated", "hedge-born", "hasty-witted", "half-faced", "guts-griping", "full-gorged", "fool-born", "folly-fallen", "fly-bitten", "flap-mouthed", "fen-sucked", "fat-kidneyed", "elf-skinned", "earth-vexing", "dread-bolted", "doghearted", "dizzy-eyed", "dismal-dreaming", "crook-pated", "common-kissing", "clay-brained", "clapper-clawed", "boil-brained", "beetle-headed", "beef-witted", "bat-fowling", "base-court"];
 var third_word = ["wagtail", "whey-face", "vassal", "varlet", "strumpet", "skainsmate", "scut", "ratsbane", "pumpion", "puttock", "pignut", "pigeon-egg", "nut-hook", "mumble-news", "moldwarp", "miscreant", "minnow", "measle", "mammet", "malt-worm", "maggot-pie", "lout", "lewdster", "joithead", "hugger-mugger", "horn-beast", "hedge-pig", "harpy", "haggard", "gudgeon", "giglet", "fustilarian", "foot-licker", "flirt-gill", "flax-wench", "flap-dragon", "dewberry", "death-token", "codpiece", "coxcomb", "clotpole", "clack-dish", "canker-blossom", "bum-bailey", "bugbear", "boar-pig", "bladder", "barnacle", "baggage", "apple-john"];
 
 // YOUR CODE HERE
+
+ function generateInsult(num) {
+   if (num === 1) {
+     return first_word[Math.round(Math.random()*50)];
+   } else if (num === 2) {
+     return first_word[Math.round(Math.random()*50)] + ' ' + second_word[Math.round(Math.random()*50)];
+   } else if (num === 3) {
+     return first_word[Math.round(Math.random()*50)] + ' ' + second_word[Math.round(Math.random()*50)]+ ' ' + third_word[Math.round(Math.random()*50)];
+   } else {
+     var insultString = '';
+     for (i=1; i<=num; i++) {
+       var randomWord = first_word[Math.round(Math.random()*50)] + ' '
+       insultString = insultString.concat(randomWord)
+     }
+   }
+   return insultString;
+ }
+
+function generateInsult() {
+  var userInput = prompt('Please name a person:');
+  $("h1").append('Why ' + userInput + ', you ' + first_word[Math.round(Math.random()*50)] + ' ' + second_word[Math.round(Math.random()*50)]+ ' ' + third_word[Math.round(Math.random()*50)] + '!');
+};
+generateInsult();
