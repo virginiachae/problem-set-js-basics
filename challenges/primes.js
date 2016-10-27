76ba7a4838a577b72496a2a6f59919aa416258a2
virginiachae <chae.virginia@gmail.com>
2016-10-26 21:20:29 -0700

    attemptede to do primes, not finished yet

--- challenges/primes.js
+++ challenges/primes.js
@@ -10,3 +10,44 @@
 */
 
 // YOUR CODE HERE
+function isPrime(value) {
+    for(var i = 2; i < value; i++) {
+        if(value % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+//attempt at second
+  function isPrime(value) {
+      var primeArray = [];
+      var testNum = 2
+      while (testNum <= value)
+        for(var i = 2; i < value; i++) {
+            if(testNum % i === 0) {
+                return false;
+                testNum += 1;
+            } else {
+          primeArray.push(testNum);
+          testNum += 1;
+        }
+        return primeArray;
+    }
+  }
+
+
+  //code found online
+  function getPrimes(max) {
+    var sieve = [], i, j, primes = [];
+    for (i = 2; i <= max; ++i) {
+        if (!sieve[i]) {
+            // i has not been marked -- it is prime
+            primes.push(i);
+            for (j = i << 1; j <= max; j += i) {
+                sieve[j] = true;
+            }
+        }
+    }
+    return primes;
+}
