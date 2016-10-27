8dd85f063b76bd6e6c53db8deca7cb556537ef4a
virginiachae <chae.virginia@gmail.com>
2016-10-26 20:35:40 -0700

    finished bottle challenge

--- challenges/bottles-of-beer-song.js
+++ challenges/bottles-of-beer-song.js
@@ -22,3 +22,26 @@
 */
 
 // YOUR CODE HERE
+var bottles = 5;
+while (bottles >= 0) {
+  if (bottles === 0) {
+    console.log('No more');
+    break;
+  } else if (bottles === 1) {
+    console.log(bottles + " bottle of beer on the wall " + bottles +
+    " bottle of beer! Take one down and pass it around, no more bottles of beer on the wall...");
+    bottles -= 1;
+  } else if (bottles === 2){
+    var newBottle = bottles -1
+    console.log(bottles + " bottles of beer on the wall " + bottles +
+    " bottles of beer! Take one down and pass it around, " + newBottle +
+    " bottle of beer on the wall...");
+    bottles -= 1;
+  } else {
+  var newBottle = bottles -1
+  console.log(bottles + " bottles of beer on the wall " + bottles +
+  " bottles of beer! Take one down and pass it around, " + newBottle +
+  " bottles of beer on the wall...");
+  bottles -= 1;
+  }
+}
